5b9e8432d4362b5c31a68bb8cf7292bb2afade81
farha-abdullah <farhan.abdullah94@gmail.com>
2023-10-09 13:53:46 +0200

    structured project

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,24 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+import HomeLayout from './Layout/HomeLayout';
+import Home from './Pages/Home/Home';
+import Error from './Error/Error';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <div>Hello world!</div>,
+		element: <HomeLayout></HomeLayout>,
+		children: [
+			{
+				path: '/',
+				element: <Home></Home>,
+			},
+		],
+	},
+	{
+		path: '*',
+		element: <Error></Error>,
 	},
 ]);
 
