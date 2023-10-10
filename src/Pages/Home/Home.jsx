b38e0be2ad070ee813f38a818fcc7881ad8b9044
farha-abdullah <farhan.abdullah94@gmail.com>
2023-10-10 12:54:15 +0200

    coomented

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,9 +7,9 @@ const Home = () => {
 		<div>
 			<Banner></Banner>
 			<Story></Story>
-			<div className='w-[300px] h-[200px] relative'>
+			{/* <div className='w-[300px] h-[200px] relative'>
 				<div className='bg-element'></div>
-			</div>
+			</div> */}
 		</div>
 	);
 };
