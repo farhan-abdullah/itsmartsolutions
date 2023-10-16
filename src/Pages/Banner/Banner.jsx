2d1940539ab6ab3bc39a1e04a439dda9d0da3a7c
monsur776e <monsuralam8897@gmail.com>
2023-10-16 11:28:58 +0600

    banner title change

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -24,9 +24,9 @@ const Banner = () => {
             className="max-w-[100%] md:h-[80vh] py-20 md:py-0 mx-auto flex justify-center items-center text-center md:text-start  text-white"
             style={bgStyle}>
             <div className=" w-[720px] mx-auto ">
-               <h2 className="md:text-[60px] text-[40px] leading-none tracking-tight md:font-bold font-semibold">
-                  HOME SERVICES CONTACT Quality Digital Services You Really
-                  Need!
+               <h2 className="md:text-[50px] text-[40px] capitalize leading-none tracking-tight md:font-bold font-semibold">
+                  Revolutionizing Your Online Landscape with Comprehensive
+                  Digital Solutions
                </h2>
                <p className="text-[16px] md:text-[18px]  mt-5">
                   We build and transform businesses by launching market-leading
