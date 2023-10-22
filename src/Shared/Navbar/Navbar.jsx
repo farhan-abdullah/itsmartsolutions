e81fbfd7a4cbe9d5f09b749c89bbc0da0e768591
monsur776e <monsuralam8897@gmail.com>
2023-10-23 01:37:42 +0600

    chatbot test

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -49,23 +49,16 @@ const Navbar = () => {
                   </label>
                   <ul
                      tabIndex={0}
-                     className={`menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52`}>
+                     className={`menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100  w-52`}>
                      <li>
-                        <a>Item 1</a>
+                        <a>Home</a>
                      </li>
-                     {/* <li>
-                        <a>Parent</a>
-                        <ul className="p-2">
-                           <li>
-                              <a>Submenu 1</a>
-                           </li>
-                           <li>
-                              <a>Submenu 2</a>
-                           </li>
-                        </ul>
-                     </li> */}
+
                      <li>
-                        <a>Item 3</a>
+                        <a>Services</a>
+                     </li>
+                     <li>
+                        <a>Contact</a>
                      </li>
                   </ul>
                </div>
